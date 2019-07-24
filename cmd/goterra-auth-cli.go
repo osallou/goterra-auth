a1229ba0bd3d2d039b72889a4a4257859c218c35
Olivier Sallou <olivier.sallou@irisa.fr>
2019-07-24 13:42:07 +0200

    add cmd to update user

--- cmd/goterra-auth-cli.go
+++ cmd/goterra-auth-cli.go
@@ -20,6 +20,7 @@ import (
 
 	terrautils "github.com/osallou/goterra-auth/lib/utils"
 	terraConfig "github.com/osallou/goterra-lib/lib/config"
+	terraUser "github.com/osallou/goterra-lib/lib/user"
 )
 
 // Version of server
@@ -35,6 +36,54 @@ type Options struct {
 	Password *string
 	Admin    *bool
 	Super    *bool
+}
+
+// Update updates user info
+var Update = func(options Options) bool {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	filter := bson.M{
+		"uid": *options.UID,
+	}
+
+	var userdb terraUser.User
+	err := userCollection.FindOne(ctx, filter).Decode(&userdb)
+	if err != nil {
+		log.Printf("User not found\n")
+		return false
+	}
+
+	toUpdate := bson.M{
+		"admin":     *options.Admin,
+		"superuser": *options.Super,
+	}
+
+	if *options.Password != "" {
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(*options.Password), bcrypt.DefaultCost)
+		if err != nil {
+			log.Println(err)
+			return false
+		}
+		toUpdate["password"] = string(hashedPassword)
+	}
+	if *options.Email != "" {
+		toUpdate["email"] = *options.Email
+	}
+
+	updateData := bson.M{
+		"$set": toUpdate,
+	}
+
+	var updatedUser terraUser.User
+	err = userCollection.FindOneAndUpdate(ctx, filter, updateData).Decode(&updatedUser)
+	if err != nil {
+		log.Printf("Error updating user: %s", err)
+		return false
+	}
+	log.Printf("User updated")
+
+	return true
 }
 
 // Register manages authentication
@@ -74,6 +123,7 @@ var Register = func(options Options) bool {
 func main() {
 
 	var register bool
+	var update bool
 	options := Options{}
 	options.UID = flag.String("uid", "", "User identifier")
 	options.Password = flag.String("password", "", "User password")
@@ -81,13 +131,14 @@ func main() {
 	options.Admin = flag.Bool("admin", false, "Is administrator?")
 	options.Super = flag.Bool("super", false, "Is super user?")
 	flag.BoolVar(&register, "register", false, "register a user")
+	flag.BoolVar(&update, "update", false, "update user info")
 	flag.Parse()
 	if *options.UID == "" || *options.Password == "" {
 		fmt.Printf("action or uid or password is empty\n")
 		os.Exit(1)
 	}
 
-	if !register {
+	if !register && !update {
 		fmt.Printf("invalid action\n")
 		os.Exit(1)
 	}
@@ -111,6 +162,8 @@ func main() {
 
 	if register {
 		Register(options)
+	} else if update {
+		Update(options)
 	} else {
 		log.Printf("no command\n")
 	}
