2f161f04df35ca5e767b381080f7ad7295e8bc2d
Olivier Sallou <olivier.sallou@irisa.fr>
2019-08-05 10:03:17 +0200

    send message to rabbit when user logs in

--- goterra-auth.go
+++ goterra-auth.go
@@ -20,6 +20,7 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+	"github.com/streadway/amqp"
 	"go.mongodb.org/mongo-driver/bson"
 	mongo "go.mongodb.org/mongo-driver/mongo"
 	mongoOptions "go.mongodb.org/mongo-driver/mongo/options"
@@ -45,6 +46,67 @@ var Version string
 
 var mongoClient mongo.Client
 var userCollection *mongo.Collection
+
+// UserLogged is message sent when a user logs
+type UserLogged struct {
+	UID  string
+	Kind string
+}
+
+//userLoggedMessage sends a message to rabbitmq exchange
+func userLoggedMessage(uid string, kind string) error {
+	if os.Getenv("GOT_MOCK_AMQP") == "1" {
+		return nil
+	}
+	config := terraConfig.LoadConfig()
+	if config.Amqp == "" {
+		fmt.Printf("[ERROR] no amqp defined\n")
+		return fmt.Errorf("No AMQP config found")
+	}
+	conn, err := amqp.Dial(config.Amqp)
+	if err != nil {
+		log.Error().Msgf("[ERROR] failed to send message for user %s: %s\n", uid, err)
+		return err
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Error().Msgf("[ERROR] failed to connect to amqp\n")
+		return err
+	}
+
+	err = ch.ExchangeDeclare(
+		"gotuser", // name
+		"fanout",  // type
+		true,      // durable
+		false,     // auto-deleted
+		false,     // internal
+		false,     // no-wait
+		nil,       // arguments
+	)
+	if err != nil {
+		log.Error().Msgf("[ERROR] failed to connect to open exchange\n")
+		return err
+	}
+
+	msg := &UserLogged{UID: uid, Kind: kind}
+	body, _ := json.Marshal(msg)
+	err = ch.Publish(
+		"gotuser", // exchange
+		"",        // routing key
+		false,     // mandatory
+		false,     // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	if err != nil {
+		log.Error().Msgf("[ERROR] failed to send message\n")
+		return err
+	}
+	return nil
+}
 
 // CheckTokenForDeployment checks JWT token
 func CheckTokenForDeployment(authToken string) (user terraUser.User, err error) {
@@ -528,6 +590,8 @@ var LoginHandler = func(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	userLoggedMessage(user.UID, user.Kind)
+
 	resp := make(map[string]interface{})
 	resp["token"] = string(token)
 	resp["apikey"] = user.APIKey
@@ -694,6 +758,8 @@ func main() {
 			log.Error().Str("user", userInfo["email"]).Msg("User already exists\n")
 		}
 
+		userLoggedMessage(loggedUser.UID, loggedUser.Kind)
+
 		loggedUser.Password = ""
 		userJSON, _ := json.Marshal(loggedUser)
 		token, tokenErr := terraToken.FernetEncode(userJSON)
@@ -782,6 +848,8 @@ func main() {
 		} else {
 			log.Error().Str("user", userInfo["sub"]).Msg("User already exists\n")
 		}
+
+		userLoggedMessage(loggedUser.UID, loggedUser.Kind)
 
 		loggedUser.Password = ""
 		userJSON, _ := json.Marshal(loggedUser)
